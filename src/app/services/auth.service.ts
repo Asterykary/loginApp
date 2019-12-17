eff8729c1f78f569259490e87f62f9f9a81d7e4c
Karina <k_ale@outlook.com>
2019-12-17 14:14:59 -0300

    Añadiendo servicio para firebase

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url = 'https://identitytoolkit.googleapis.com/v1';
+  private apikey = 'AIzaSyAGoBm3gC-xqccSf4FuukB3drvuUVM9aPQ';
+
+  // Crear nuevo usuario
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+  // login
+  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+  constructor(private http: HttpClient) { }
+
+  logout() {
+
+  }
+
+  login(usuario: UsuarioModel) {
+
+  }
+
+  nuevoUsuario( usuario: UsuarioModel) {
+
+  }
+}
