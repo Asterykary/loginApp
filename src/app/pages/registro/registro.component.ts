251046e7ba2c8639ca9f41100ec064f890de687c
Karina <k_ale@outlook.com>
2019-12-18 11:33:00 -0300

    Sweetalert2 | navegación home

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from '../../models/usuario.model';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -13,7 +15,8 @@ export class RegistroComponent implements OnInit {
   usuario: UsuarioModel;
 
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
@@ -24,12 +27,28 @@ export class RegistroComponent implements OnInit {
 
     if (form.invalid) {return; }
 
+    Swal.fire ({
+      allowOutsideClick: false,
+      text: 'Espere por favor...',
+      icon: 'info',
+    });
+    Swal.showLoading();
+
     this.auth.nuevoUsuario(this.usuario)
     .subscribe( resp => {
 
       console.log(resp);
+      Swal.close();
+      this.router.navigateByUrl('/home');
     }, (error) => {
       console.log(error);
+      Swal.fire ({
+        allowOutsideClick: false,
+        text: 'Espere por favor...',
+        icon: 'info',
+      });
+      Swal.showLoading();
+
     });
    }
 
